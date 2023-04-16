f4d149362d36755867fe54f4d046b7c741dab221
Avwerosuoghene_Darhare-Igben <kesuion1@gmail.com>
2023-04-16 21:29:04 +0100

    test

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,7 +7,7 @@ import { ExpirationCompleteListener } from "./events/listeners/expiration-comple
 import { PaymentCreatedListener } from "./events/listeners/payment-created-listener";
 
 const start = async () => {
-  console.log('starting...!!!');
+  console.log('starting..');
   console.log(process.env.JWT_KEY);
   // Checks for jwt key
   if (!process.env.JWT_KEY) {
